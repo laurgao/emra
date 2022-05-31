8a8a2793b537e5588e1916b5aaaae94e5a58f8ff
Laura Gao <lauragao86868@gmail.com>
2022-05-31 13:14:23 -0400

    Basic setup

---
+++ Frame.java
@@ -0,0 +1,26 @@
+
+/* Frame class establishes the frame (window) for the game
+It is a child of JFrame because JFrame manages frames
+Runs the constructor in GamePanel class
+
+*/
+import java.awt.*;
+import javax.swing.*;
+
+public class Frame extends JFrame {
+
+    public Frame() {
+        Screen content = new Screen(); // run GamePanel constructor
+        this.add(content);
+        this.setTitle("Pong"); // set title for frame
+        this.setResizable(false); // frame can't change size
+        this.setBackground(Color.white);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // X button will stop program execution
+        this.pack(); // makes components fit in window - don't need to set JFrame size, as it will
+                     // adjust accordingly
+                     // this makes the frame the size of the gamepanel.
+        this.setVisible(true); // makes window visible to user
+        this.setLocationRelativeTo(null);// set window in middle of screen
+    }
+
+}
---
+++ Screen.java
@@ -0,0 +1,118 @@
+
+/* GamePanel class acts as the main "game loop" - continuously runs the game and calls whatever needs to be called
+
+Child of JPanel because JPanel contains methods for drawing to the screen
+
+Implements KeyListener interface to listen for keyboard input
+
+Implements Runnable interface to use "threading" - let the game do two things at once
+
+*/
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Screen extends JPanel implements Runnable, KeyListener {
+
+    // dimensions of window
+    public static final int W = 500; // width of window
+    public static final int H = 500; // height of window
+
+    public String currentScreen; // current screen being displayed
+
+    public Thread gameThread;
+
+    public Screen() {
+
+        // add the MousePressed method from the MouseAdapter - by doing this we can
+        // listen for mouse input.
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+
+            }
+        });
+
+        this.setPreferredSize(new Dimension(W, H));
+        this.setFocusable(true); // make everything in this class appear on the screen
+        this.addKeyListener(this); // start listening for keyboard input
+
+        // make this class run at the same time as other classes (without this each
+        // class would "pause" while another class runs). By using threading we can
+        // remove lag, and also allows us to do features like display timers in real
+        // time!
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    // paint is a method in java.awt library that we are overriding.
+    // It is called automatically in the background in order to update what
+    // appears in the window.
+    public void paint(Graphics g) {
+        // use double buffering - draw images OFF the screen, then move the image on
+        // screen
+        Image image = createImage(W, H); // draw off screen
+        Graphics graphics = image.getGraphics();
+        draw(graphics);// update the positions of everything on the screen
+        g.drawImage(image, 0, 0, this); // move the image on the screen
+
+    }
+
+    // call the draw methods in each class to update positions as things move
+    public void draw(Graphics g) {
+        // draw the background
+        Color bgColor = new Color(52, 211, 153);
+        g.setColor(bgColor);
+        g.fillRect(0, 0, W, H);
+
+    }
+
+    // call the move methods in other classes to update positions
+    // this method is constantly called from run(). By doing this, movements appear
+    // fluid and natural.
+    public void move() {
+
+    }
+
+    // run() method is what makes the game continue running without end. It calls
+    // other methods to move objects and update the screen
+    public void run() {
+        // the CPU runs our game code too quickly - we need to slow it down! The
+        // following lines of code "force" the computer to get stuck in a loop for short
+        // intervals between calling other methods to update the screen.
+        long lastTime = System.nanoTime();
+        final double amountOfTicks = 60;
+        final double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long now;
+
+        while (true) { // this is the infinite game loop
+            now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            // only move objects around and update screen if enough time has passed
+            if (delta >= 1) {
+                move();
+                repaint();
+                delta--;
+            }
+
+        }
+    }
+
+    // if a key is pressed, we'll send it over to the player paddle object for
+    // processing
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    // if a key is released, we'll send it over to the player paddle object for
+    // processing
+    public void keyReleased(KeyEvent e) {
+    }
+
+    // left empty because we don't need it; must be here because it is required to
+    // be overridded by the KeyListener interface
+    public void keyTyped(KeyEvent e) {
+
+    }
+}
